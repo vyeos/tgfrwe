66c096e4fd376e5754a0530305e0af552c85aef8
vyeos <rudranpatel0980@gmail.com>
2026-02-10 16:09:02 +0530

    add: connect page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: vercel(),
 });
